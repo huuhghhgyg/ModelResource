d96029998ac69e0cc3dd7036ac3465a517351f86
huuhghhgyg <huuhghhgyg@outlook.com>
2024-01-16 16:35:20 +0800

    TableString显示键值

--- libs/TableString.lua
+++ libs/TableString.lua
@@ -6,26 +6,47 @@ function TableString(t)
 
     -- 剩下table类型
     local str = '{'
-    for k, v in ipairs(t) do
-        -- print('detecting table'..k,'str='..str)
-        if type(v) == 'table' then
-            str = str .. TableString(v)
+
+    -- 迭代table中的内容
+    local keys = 0
+    local indices = 0
+    for k, v in pairs(t) do
+        if type(k) == 'number' then
+            -- 键值为index
+            if type(v) == 'table' then
+                str = str .. TableString(v)
+            else
+                str = str .. v
+            end
+            indices = indices + 1
         else
-            str = str .. v
+            -- 键值为key
+            if type(v) == 'table' then
+                str = str .. k .. '=' .. TableString(v)
+            else
+                str = str .. k .. '=' .. v
+            end
+            keys = keys + 1
         end
 
-        if k < #t then
-            str = str .. ', '
-        end
+        str = str .. ', '
     end
+
+    if indices + keys > 0 then
+        str = string.sub(str, 1, -3)
+    end
+
     str = str .. '}'
 
     return str
 end
 
 -- 示例代码
--- local collection = {'a',{1,2},{'hi', {'Anna', 'Bell'}},{{'x','y','z'},{1,2,3}}}
--- print(TableString(collection))
+-- local collection = {'a',{1,2},{'hi', {'Anna', 'Bell', name='AnnaBell'}},{{'x','y','z'},{1,2,3}}}
+-- local collection = {a='1',b='2', 'x', 'y', 'z'}
+-- local collection = {1,2,3}
 -- print(TableString('a'))
 -- print(TableString(2))
 -- print(TableString({}))
+
+-- print(TableString(collection))
