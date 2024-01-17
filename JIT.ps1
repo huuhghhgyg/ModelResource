998ea5f2df5df29ae04e6889760d28a2bffac0b1
huuhghhgyg <huuhghhgyg@outlook.com>
2024-01-17 16:35:38 +0800

    更新开发脚本
    如果没有相关插件可以使用

--- JIT.ps1
+++ JIT.ps1
@@ -1,2 +1,4 @@
 # 用于JIT开发
+# Start-Process powershell.exe -ArgumentList "`$host.ui.RawUI.WindowTitle='Local Web Server';python -m http.server 8080"
+# Start-Process powershell.exe -ArgumentList "start 'http://localhost:8080'"
 npx tailwindcss -o style.css --watch
